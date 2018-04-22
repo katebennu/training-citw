ea21b178c6d30c255c7d2951657d025079b2e84a
katebennu <kate.dorrer@gmail.com>
2018-04-22 13:03:44 +0300

    exercise 20, save file works

--- Citw/App.js
+++ Citw/App.js
@@ -10,8 +10,13 @@ import {
   StyleSheet,
   Text,
   View,
-  AsyncStorage
+  AsyncStorage,
+  Button,
+  Alert
 } from 'react-native';
+import { getBatteryLevel } from 'react-native-fs';
+
+// Render the contents of RNFS.DocumentDirectoryPath on the screen
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -22,21 +27,61 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
-  componentDidMount() {
-    const promise = AsyncStorage.getItem('myname');
+  constructor(props) {
+    super(props);
+    this.state = { addresses: [] }
   }
+  async componentDidMount() {
+    var RNFS = require('react-native-fs');
+
+    RNFS.readDir(RNFS.DocumentDirectoryPath) 
+          .then((result) => {
+            debugger;
+            console.warn('GOT RESULT', result);
+
+            // stat the first file
+            return Promise.all([RNFS.stat(result[0].path), result[0].path]);
+          })
+          .then((statResult) => {
+            // debugger;
+            if (statResult[0].isFile()) {
+              // if we have a file, read it
+              console.warn(statResult[1]);
+            }
+
+          });
+
+
+    fetch('https://staltz.com/g.txt')
+      .then(response => response.text())
+      .then(fileContent => {
+        // console.warn(fileContent);
+        var path = RNFS.DocumentDirectoryPath + '/r.txt';
+        // write the file
+        RNFS.writeFile(path, fileContent, 'utf8')
+          .then((success) => {
+            console.warn('FILE WRITTEN!');
+          })
+          .catch((err) => {
+            console.warn(err.message);
+          });
+
+        // get a list of files and directories in the main bundle
+        
+
+      });
+    }
+  onPress = () => {
+          AsyncStorage.setItem('myname', 'K');
+        }
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Very Welcome to React Native, bro
+          return(
+      <View style = { styles.container } >
+              <Text style={styles.welcome}>
+                Files:
+      
         </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+              <Button title="Press" onPress={this.onPress} />
       </View>
     );
   }
